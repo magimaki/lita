9142cf6cfe381f78bb66afcee273b69ae9b5c73b
Alodi <magimaki@yeah.net>
2022-11-15 20:17:44 +0800

    feat(cli): implement remind command

--- cli/remind_command.go
+++ cli/remind_command.go
@@ -2,6 +2,8 @@ package cli
 
 import (
 	"fmt"
+	"lita"
+	"lita/model"
 	"os"
 
 	"github.com/spf13/cobra"
@@ -18,27 +20,70 @@ var remindCmd = &cobra.Command{
 	Use:   "remind",
 	Short: "Set event reminder",
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println(remindCmdFlagDate)
-		fmt.Println(remindCmdFlagTime)
-		fmt.Println(remindCmdFlagEvent)
-		fmt.Println(remindCmdFlagAddress)
+		if len(remindCmdFlagDate) != 8 {
+			fmt.Println(lita.NewError(lita.ErrCmdInput, "invalid event date format, need `yyyymmdd`", nil))
+			os.Exit(1)
+		}
+
+		if len(remindCmdFlagTime) != 4 && len(remindCmdFlagTime) != 0 {
+			fmt.Println(remindCmdFlagTime)
+			fmt.Println(len(remindCmdFlagTime))
+			fmt.Println(lita.NewError(lita.ErrCmdInput, "invalid event time format, need `hhmm`", nil))
+			os.Exit(1)
+		}
+
+		var onlyDateEvent bool
+		if len(remindCmdFlagTime) == 0 {
+			remindCmdFlagTime = "0800"
+			onlyDateEvent = true
+		}
+
+		year := remindCmdFlagDate[0:4]
+		month := remindCmdFlagDate[4:6]
+		day := remindCmdFlagDate[6:]
+		hour := remindCmdFlagTime[0:2]
+		minute := remindCmdFlagTime[2:]
+		timeStr := fmt.Sprintf("%s-%s-%s %s:%s", year, month, day, hour, minute)
+		timeObj, err := model.StrToTime(timeStr)
+		if err != nil {
+			fmt.Println(lita.NewError(lita.ErrCmdInput,
+				"invalid event date or time input, need `yyyymmdd` (m 01-12, d 01-31) "+
+					"and `hhmm` (h 01-23 m 01-59) with only digit", err))
+			os.Exit(1)
+		}
+		reminder := model.Reminder{
+			Year:          year,
+			Month:         month,
+			Day:           day,
+			Hour:          hour,
+			Minute:        minute,
+			Weekday:       timeObj.Weekday().String(),
+			TimeStr:       timeStr,
+			Time:          timeObj,
+			Event:         remindCmdFlagEvent,
+			Address:       remindCmdFlagAddress,
+			OnlyDateEvent: onlyDateEvent,
+		}
+		fmt.Println(reminder)
 	},
 }
 
 func init() {
-	remindCmd.Flags().StringVarP(&remindCmdFlagDate, "date", "d", "", "event date: yyyymmdd")
-	remindCmd.Flags().StringVarP(&remindCmdFlagTime, "time", "t", "0800", "event time: hhmm")
-	remindCmd.Flags().StringVarP(&remindCmdFlagEvent, "event", "e", "", "Source directory to read from")
-	remindCmd.Flags().StringVarP(&remindCmdFlagAddress, "address", "a", "", "Source directory to read from")
+	remindCmd.Flags().StringVarP(&remindCmdFlagDate,
+		"date", "d", "", "event date: yyyymmdd")
+	remindCmd.Flags().StringVarP(&remindCmdFlagTime,
+		"time", "t", "", "event time: hhmm")
+	remindCmd.Flags().StringVarP(&remindCmdFlagEvent,
+		"event", "e", "", "Source directory to read from")
+	remindCmd.Flags().StringVarP(&remindCmdFlagAddress,
+		"address", "a", "", "Source directory to read from")
 
-	err := remindCmd.MarkFlagRequired("date")
-	if err != nil {
+	if err := remindCmd.MarkFlagRequired("date"); err != nil {
 		fmt.Println(err)
 		os.Exit(1)
 	}
 
-	err = remindCmd.MarkFlagRequired("event")
-	if err != nil {
+	if err := remindCmd.MarkFlagRequired("event"); err != nil {
 		fmt.Println(err)
 		os.Exit(1)
 	}
